36ea2a58b2ee839359c97b64f60f7d8dc4009d29
yefeng <silend-wind@qq.com>
2025-07-30 18:28:05 +0800

    feat(yefeng-ai-code): 新增yefeng-ai-code项目后端初始化
    1. 引入 hutool、Knife4j依赖
    2. 实现通用基础代码，自定义异常，全局异常处理等
    3. 实现健康检查接口

---
+++ yefeng-ai-codex/yefeng-ai-code/src/main/java/com/yefeng/yefengaicode/YefengAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.yefeng.yefengaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YefengAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YefengAiCodeApplication.class, args);
+    }
+
+}
---
+++ yefeng-ai-codex/yefeng-ai-code/src/main/java/com/yefeng/yefengaicode/controller/HealthController.java
@@ -0,0 +1,8 @@
+package com.yefeng.yefengaicode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/health")
+public class HealthController {
+
+}
