6c68013b464ca595088a4b3449474b2789f292f6
yefeng <silend-wind@qq.com>
2025-07-31 22:31:11 +0800

    feat(yefeng-ai-code): 新增健康接口
    1.增加get请求接口

--- yefeng-ai-codex/yefeng-ai-code/src/main/java/com/yefeng/yefengaicode/controller/HealthController.java
+++ yefeng-ai-codex/yefeng-ai-code/src/main/java/com/yefeng/yefengaicode/controller/HealthController.java
@@ -1,8 +1,16 @@
 package com.yefeng.yefengaicode.controller;
 
+import com.yefeng.yefengaicode.common.BaseResponse;
+import com.yefeng.yefengaicode.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/health")
 public class HealthController {
-
+    @GetMapping("/get")
+    public BaseResponse health(String name) {
+        return ResultUtils.success("ok");
+    }
 }
