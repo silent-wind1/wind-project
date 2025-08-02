fbb77ec1471cc13b4adc1c518cf2d65706ce0e87
yefeng <silend-wind@qq.com>
2025-08-02 13:51:08 +0800

    feat(yefeng-ai-code): 更改JsonConfig配置
    1.更改jacksonObjectMapper的配置，添加LocalDateTime、LocalDate、LocalTime的序列化yyyy-MM-dd HH:mm:ss、yyyy-MM-dd、HH:mm:ss的格式化

--- yefeng-ai-codex/yefeng-ai-code/src/main/java/com/yefeng/yefengaicode/config/JsonConfig.java
+++ yefeng-ai-codex/yefeng-ai-code/src/main/java/com/yefeng/yefengaicode/config/JsonConfig.java
@@ -3,30 +3,47 @@ package com.yefeng.yefengaicode.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Spring MVC Json 配置
  */
+@Slf4j
 @JsonComponent
 public class JsonConfig {
 
     /**
-     * 添加 Long 转换 json 精度丢失的配置
+     * 序列化配置
      *
      * @param builder 构造器
      * @return ObjectMapper
      */
     @Bean
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        log.info("jacksonObjectMapper 进行Long转换成String");
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         SimpleModule simpleModule = new SimpleModule();
         //Long类型转String类型
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         //long类型转String类型
         simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        //日期格式处理
+        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        // 处理LocalDate
+        simpleModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        // 处理LocalTime
+        simpleModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
         objectMapper.registerModule(simpleModule);
         return objectMapper;
     }
